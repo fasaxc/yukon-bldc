2fca4d94c2b2c941a142a40fcde056d9f603b0de
Shaun Crampton <shaun@cantab.net>
2024-08-03 12:21:49 +0100

    Refactor: move everything to Motor class.
    - Parameterise on Yukon slot number.
    - Calculate addresses for PWM/FIFO etc access.
    - General clean up.

--- main.py
+++ main.py
@@ -11,6 +11,290 @@ import gc
 import rp2
 from pimoroni_yukon import Yukon
 
+pin_debug = Pin(26, Pin.OUT)
+
+
+# Variables
+yukon = Yukon()     # A new Yukon object
+led_state = False   # The state of the LED
+
+# Wrap the code in a try block, to catch any exceptions (including KeyboardInterrupt)
+
+
+# Hack to make VSCode happy with viper pointers.
+ptr32 = ptr16 = lambda x:x
+
+stop = False
+
+class Motor:
+    _lut_len : int = 4096
+    _lut = None
+
+    def __init__(
+            self, 
+            pio_sm_num: int,
+            slot_num: int,
+            pwm_freq: int = 40000,
+        ) -> None:
+        """
+        Parameters:
+          pio_sm_num: PIO StateMachine number to use.
+          slot_num:   Yukon slot number (used to determine which 
+                      group of GPIO pins/PWMs to use).  
+
+                      slot 1 -> GPIO 0-3,   PWM 0-1
+                      slot 2 -> GPIO 4-7,   PWM 2-3
+                      slot 3 -> GPIO 8-11,  PWM 4-5
+                      slot 4 -> GPIO 12-15, PWM 6-7
+                      slot 5 -> GPIO 16-19, PWM 0-1 (clash with slot 1)
+                      slot 6 -> GPIO 20-23, PWM 2-3 (clash with slot 2)
+        """
+        # One-off look-up-table initialisation.
+        self._init_lut()
+
+        # We use the first three GPIOs in the block for the motor outputs.
+        first_gpio_num = (slot_num-1) * 4
+        self._motor_pwms = [PWM(Pin(n)) for n in range(first_gpio_num, first_gpio_num+3)]
+        self._pwm_banks = [((slot_num-1) * 2) % 8, (((slot_num-1) * 2)+1) % 8]
+        self._pwm_duty_addrs = array.array('I')
+        self._configure_pwms(pwm_freq)
+
+        # We use the final GPIO in the slot for the sensor input. Configure it
+        # with a PIO program to read the PWM signal.
+        sensor_pin_num = first_gpio_num+3
+        self._sensor_pin = Pin(sensor_pin_num, Pin.IN, Pin.PULL_UP)
+        self._sensor_sm_num = pio_sm_num
+        if pio_sm_num < 4:
+            pio_base = 0x50200000
+        else:
+            pio_base = 0x50300000
+        self._sensor_fifo_addr = pio_base + 0x20 + (pio_sm_num % 4)
+        self._sensor_debug_addr = pio_base+0x8
+        self._sensor_rx_stall_mask = 1 << (pio_sm_num % 4)
+
+        self._sensor_sm = rp2.StateMachine(
+            pio_sm_num,
+            pio_read_pwm, 
+            in_base=self._sensor_pin, 
+            jmp_pin=self._sensor_pin,
+        )
+        self._sensor_sm.active(1)
+
+        # Calibration values.
+        self.angle_offset : int = 0
+        self.num_pole_pairs : int = 0
+
+        # Pole angle at which we drive the coils vs the rotor position.
+        # 1024 = +90 degrees
+        # 3072 = -90 degrees
+        self.drive_offset :int = 1024
+
+    @classmethod
+    def _init_lut(cls):
+        if cls._lut is not None:
+            return 
+        third = 4096 // 3
+        # This fraction of the PWM duty cycle should match the switching 
+        # delay of the motor driver.
+        min_on_fraction = 0.016
+        scale_factor = 65535 * (1-min_on_fraction) / 2
+        two_pi = 2 * math.pi
+        cls._lut = array.array('H')
+        for i in range(cls._lut_len):
+            sin = math.sin(i * two_pi / cls._lut_len)
+            sin_scaled = (sin+1)*scale_factor
+            sin_offset = sin_scaled + min_on_fraction
+            cls._lut.append(int(sin_offset))
+
+    def _configure_pwms(self, f):
+        # Start with the basic configuration via the library.
+        for pwm in self._motor_pwms:
+            pwm.freq(f*2) # factor of 2 due to phase accurate mode.
+        
+        # Now tweak some settings via registers because the library 
+        # lacks support...
+        pwm_base = 0x40050000
+
+        # Look up the PWM_EN register, which allows us to enable/disable
+        # both banks together.  This lets us sync the PWMs.
+        pwm_en = pwm_base + 0xa0
+        set_offset = 0x2000
+        clr_offset = 0x3000
+        pwm_en_set = pwm_en + set_offset
+        pwm_en_clr = pwm_en + clr_offset
+        
+        # Calculate mask for our banks.
+        pwm_en_mask = 0
+        for bank in self._pwm_banks:
+            pwm_en_mask |= 1<<bank
+        
+        # Disable our PWM banks.  Whiting to the "clear" version of the
+        # register clears the bits in the mask.
+        machine.mem32[pwm_en_clr] = pwm_en_mask
+
+        for bank in self._pwm_banks:
+            # Store off the address for the ISR.
+            bank_base = pwm_base + 0x14 * bank
+            self._pwm_duty_addrs.append(bank_base+0x0c)
+            # Enable phase-accurate mode
+            bank_csr_set = bank_base + set_offset
+            machine.mem32[bank_csr_set] = 0x2
+            bank_ctr = bank_base + 0x8
+            # Reset the counter so that both banks start together.
+            machine.mem32[bank_ctr] = 0
+
+        # Start both PWMs together.
+        machine.mem32[pwm_en_set] = pwm_en_mask
+
+    def calibrate(self):
+        # Calibration: rotate by a quarter phase to capture the rotor.
+        # Seems to be a good angle to read off the angle offset.  Maybe
+        # because the first duty is at 100%?
+        for phase_angle in range(self._lut_len//4):
+            time.sleep_us(100)
+            pin_debug.on()
+            self._set_duty(phase_angle, 1024)
+            pin_debug.off()
+            
+        time.sleep_ms(500) # Let rotor settle.
+
+        angle1 = self._read_pwm_slow()
+        print("Calibration interval A = {} = {} deg".format(angle1, angle1*360/4096))
+
+        for phase_angle in range(self._lut_len//4, self._lut_len*9//4):
+            time.sleep_us(100)
+            pin_debug.on()
+            self._set_duty(phase_angle, 1024)
+            pin_debug.off()
+            
+        time.sleep_ms(500) # Let rotor settle.
+        angle2 = self._read_pwm_slow()
+        print("Calibration interval B = {} = {} deg".format(angle2, angle2*360/4096))
+        delta = clamp_angle(angle2-angle1)
+        print("Delta = {} = {}".format(delta, delta*360/4096))
+        num_pole_pairs = (4096*2*32//abs(delta) + 15) // 32
+        print("Pole pairs = {}".format(num_pole_pairs))
+
+        # Now we've got the number of poles, we can work out the phase offset.
+        pole_angle = 4096//4 # Angle we set when we did the measurement
+        meas_pole_angle = clamp_angle(angle1 * num_pole_pairs)
+        angle_offset = clamp_angle(pole_angle - meas_pole_angle)
+        print("Angle offset = {}".format(angle_offset))
+
+        self._set_duty(0, 0)
+
+        self.angle_offset, self.num_pole_pairs =  angle_offset, num_pole_pairs
+
+    @micropython.viper
+    def _set_duty(self, angle : int, power : int):
+        lut_ptr = ptr16(self._lut) 
+        angle = angle & 0xfff
+        self._motor_pwms[0].duty_u16((lut_ptr[angle] * power) >> 12)
+        angle2 = (angle+1365) & 0xfff
+        self._motor_pwms[1].duty_u16((lut_ptr[angle2] * power) >> 12)
+        angle3 = (angle+2731) & 0xfff
+        self._motor_pwms[2].duty_u16((lut_ptr[angle3] * power) >> 12)
+
+    def _read_pwm_slow(self) -> int:
+        """Read next value from the PWM FIFO.
+        
+        Detects FIFO overflow and resyncs with the PWM signal 
+        if needed (this takes 2-3 PWM cycles).
+
+        Returns the wheel angle in 4096ths.
+        """
+        # Drain the FIFO.
+        while self._sensor_sm.rx_fifo() > 0:
+            _ = self._sensor_sm.get()
+
+        # Then, block waiting for the next value...
+        reads_needed = 1
+        while reads_needed>0:
+            packed_value = self._sensor_sm.get()
+            reads_needed-=1
+
+            # Check for an earlier FIFO stall.  If there was a stall, the value
+            # we read might be junk.
+            if machine.mem32[self._sensor_debug_addr] & self._sensor_rx_stall_mask:
+                print("FIFO stall")
+                # Do two more reads plus the FIFO length, in case of a long GC stall.
+                reads_needed = 2+self._sensor_sm.rx_fifo()
+                machine.mem32[self._sensor_debug_addr+ 0x3000] = self._sensor_rx_stall_mask
+
+
+        # Data is actually two 16-bit counters packed into
+        # a 32-bit word.
+        raw_invl = packed_value & 0xffff
+        raw_high = (packed_value >> 16) & 0xffff
+        invl = 0xffff - raw_invl
+        high = 0xffff - raw_high
+        angle = high*4119//invl - 15
+        # TODO: <16 means "error"
+        if angle < 0:
+            angle = 0
+        if angle > 4095:
+            angle = 0
+        return angle
+
+    @micropython.viper
+    def update(self, pio):
+        pin_debug.on()
+        try:
+            fifo = ptr32(self._sensor_fifo_addr)    
+        
+            # Data is actually two 16-bit counters packed into
+            # a 32-bit word.
+            packed_value = fifo[0]
+            raw_invl = packed_value & 0xffff
+            raw_high = (packed_value >> 16) & 0xffff
+            invl = 0xffff - raw_invl
+            high = 0xffff - raw_high
+            angle = high*4119//invl - 15
+            # TODO: <16 means "error"
+            if angle < 0:
+                angle = 0
+            if angle > 4095:
+                angle = 0
+            
+            pole_angle : int = angle * int(self.num_pole_pairs) + int(self.angle_offset)
+            drive_angle : int = pole_angle + int(self.drive_offset)
+
+            power = 1024
+            lut_ptr = ptr16(self._lut)
+            tap1 = drive_angle & 0xfff
+            tap2 = (drive_angle+1365) & 0xfff
+            tap3 = (drive_angle+2731) & 0xfff
+            duty1 = (lut_ptr[tap1] * power) >> 18 # Extra shift of 6 for PWM TOP scale.
+            duty2 = (lut_ptr[tap2] * power) >> 18
+            duty3 = (lut_ptr[tap3] * power) >> 18
+
+            # Want to do this but each method call costs 10us, which
+            # is far too much.
+            # pwms[0].duty_u16(duty1)
+            # pwms[1].duty_u16(duty2)
+            # pwms[2].duty_u16(duty3)
+
+            # 20 = 2A 0x40050034 LSB
+            # 21 = 2B            MSB
+            # 22 = 3A 0x40050048
+            duty_addrs = ptr32(self._pwm_duty_addrs)
+            ptr32(duty_addrs[0])[0] = duty1 | (duty2<<16)
+            ptr32(duty_addrs[1])[0] = duty3
+        except BaseException:
+            global stop 
+            stop = True
+        pin_debug.off()
+
+
+# Clamp and angle into [-2048, 2048)
+@micropython.viper
+def clamp_angle(angle:int) -> int:
+    angle = angle % 4096
+    if angle >= 2048:
+        angle -= 4096
+    elif angle < -2048:
+        angle += 4096
+    return angle
 
 # PIO program to read a PWM signal on its input pin
 # Requires the single input pin to be mapped to both 
@@ -46,279 +330,17 @@ def pio_read_pwm():
     wrap()                       # }                       # type:ignore           
 
 
-pin_sensor = Pin(23, Pin.IN, Pin.PULL_UP)
-pwm_sm = rp2.StateMachine(
-    0, 
-    pio_read_pwm, 
-    freq=125_000_000,  
-    in_base=pin_sensor, 
-    jmp_pin=pin_sensor,
-)
-
-# Start the StateMachine's running.
-pwm_sm.active(1)
-
-pin_debug = Pin(26, Pin.OUT)
-
-
-# Variables
-yukon = Yukon()     # A new Yukon object
-led_state = False   # The state of the LED
-
-# Wrap the code in a try block, to catch any exceptions (including KeyboardInterrupt)
-
-
-
-pwms = [PWM(Pin(n)) for n in [20, 21, 22]]
-
-PWM_BASE = 0x40050000
-PWM_EN = PWM_BASE + 0xa0
-PWM_INTR = PWM_BASE + 0xa4
-
-PWM_REGS = {}
-
-for bank in range(8):
-    bank_base = PWM_BASE + 0x14*bank
-    PWM_REGS[bank] = {
-        "CSR": bank_base,
-        "CTR": bank_base + 0x08,    
-    }
-
-PWM_BANK_LO = 2
-PWM_BANK_HI = 3
-
-# Set the PWM frequency.
-f = 40000
-ivl = 0.99/f
-for pwm in pwms:
-    pwm.freq(f*2)  # *2 for phase accurate PWM
-
-# Sync PWMs. EN register mirrors enable bits for individual PWMs.
-machine.mem32[PWM_EN] = 0
-machine.mem32[PWM_REGS[PWM_BANK_LO]["CSR"]] = machine.mem32[PWM_REGS[PWM_BANK_LO]["CSR"]] | 0x2
-machine.mem32[PWM_REGS[PWM_BANK_HI]["CSR"]] = machine.mem32[PWM_REGS[PWM_BANK_HI]["CSR"]] | 0x2
-machine.mem32[PWM_REGS[PWM_BANK_LO]["CTR"]] = 0
-machine.mem32[PWM_REGS[PWM_BANK_HI]["CTR"]] = 0
-machine.mem32[PWM_EN] = (1<<2) | (1 <<3)
-
-lut_len : int = 4096
-third = 4096 // 3
-offset = 0.016
-lut = array.array('H', [
-    min(
-        int(((math.sin(i * 2 * math.pi / lut_len) + 1)/2 + offset) / (1+offset) * 65535),
-        65535
-    ) for i in range(lut_len)])
-
-_read_pwm_block_read_buf=array.array("i", [0])
-@micropython.viper
-def read_pwm_block(pwm_sm, out_buf:ptr32): # type:ignore
-    """Read a fresh value from the PWM FIFO.
-
-    Detects FIFO overflow and resyncs with the PWM signal 
-    if needed (this takes 2-3 PWM cycles).
-    
-    To avoid allocations, the values are returned via
-    the out_buf in the order high, interval.
-    """
-
-    # Check if we need to handle a FIFO overflow.
-    overflow = int(pwm_sm.rx_fifo()) == 4
-    # Drain the FIFO.
-    while 1:
-        pwm_sm.get(_read_pwm_block_read_buf)
-        if int(pwm_sm.rx_fifo()) == 0:
-            break
-    if overflow:
-        # If FIFO overflowed, then the next value will
-        # be garbage because the PIO program blocks and 
-        # restarts at a random point of a PWM cycle.
-        # Read another couple of values to get back in 
-        # sync.
-        pwm_sm.get(_read_pwm_block_read_buf)
-        pwm_sm.get(_read_pwm_block_read_buf)
-
-    buf_ptr = ptr16(_read_pwm_block_read_buf)  # type: ignore
-    high = 0xffff - buf_ptr[1]
-    invl = 0xffff - buf_ptr[0]
-    # Return via the provided buffer because returning 32
-    # bits or a tuple would allocate.
-    out_buf[0] = high
-    out_buf[1] = invl
-
-
-@micropython.viper
-def set_duty(angle : int, power : int):
-    lut_ptr = ptr16(lut)
-    angle = angle & 0xfff
-    pwms[0].duty_u16((lut_ptr[angle] * power) >> 12)
-    angle2 = (angle+1365) & 0xfff
-    pwms[1].duty_u16((lut_ptr[angle2] * power) >> 12)
-    angle3 = (angle+2731) & 0xfff
-    pwms[2].duty_u16((lut_ptr[angle3] * power) >> 12)
-
-
-@micropython.viper
-def clamp_angle(angle:int) -> int:
-    angle = angle % 4096
-    if angle >= 2048:
-        angle -= 4096
-    elif angle < -2048:
-        angle += 4096
-    return angle
-
-
-def full_calibration():
-    # Calibration: rotate by a quarter phase to capture the rotor.
-    # Seems to be a good angle to read off the angle offset.  Maybe
-    # because the first duty is at 100%?
-    for phase_angle in range(lut_len//4):
-        time.sleep_us(100)
-        pin_debug.on()
-        machine.mem32[PWM_INTR] = 0x7f
-        set_duty(phase_angle, 1024)
-        pin_debug.off()
-        
-    time.sleep_ms(500) # Let rotor settle.
-
-    pwm_buf = array.array("I", [0] * 2)
-    read_pwm_block(pwm_sm, pwm_buf)
-    high = pwm_buf[0]
-    invl = pwm_buf[1]
-    angle1 = clamp_angle(high*4096//invl)
-    print("Calibration interval A = {} / {} = {} = {}".format(high, invl, angle1, angle1*360/4096))
-
-    for phase_angle in range(lut_len//4, lut_len*9//4):
-        time.sleep_us(100)
-        pin_debug.on()
-        machine.mem32[PWM_INTR] = 0x7f
-        set_duty(phase_angle, 1024)
-        pin_debug.off()
-        
-    time.sleep_ms(500) # Let rotor settle.
-    read_pwm_block(pwm_sm, pwm_buf)
-    high2 = pwm_buf[0]
-    invl2 = pwm_buf[1]
-    angle2 = clamp_angle(high2*4096//invl2)
-    print("Calibration interval B = {} / {} = {} = {}".format(high2, invl2, angle2, angle2*360/4096))
-    delta = clamp_angle(angle2-angle1)
-    print("Delta = {} = {}".format(delta, delta*360/4096))
-    num_pole_pairs = (4096*2*32//abs(delta) + 15) // 32
-    print("Pole pairs = {}".format(num_pole_pairs))
-
-    # Now we've got the number of poles, we can work out the phase offset.
-    pole_angle = 4096//4 # Angle we set when we did the measurement
-    meas_pole_angle = clamp_angle(angle1 * num_pole_pairs)
-    angle_offset = clamp_angle(pole_angle - meas_pole_angle)
-    print("Angle offset = {}".format(angle_offset))
-
-    for pwm in pwms:
-        pwm.duty_u16(0)
-
-    return angle_offset, num_pole_pairs
-
-
-stop = False
-
-class Motor:
-    def __init__(self) -> None:
-        # Pre-allocated buffer for use by ISR.
-        self.sm_buf = array.array('i',[0])
-
-        self.angle_offset : int = 0
-        self.num_pole_pairs : int = 0
-
-        self.drive_offset :int = 1024
-        self.a_was_pressed : bool = False
-
-    def calibrate(self):
-        self.angle_offset, self.num_pole_pairs = full_calibration()
-
-    @micropython.viper
-    def _read_pwm(self) -> int:
-        """Read next value from the PWM FIFO.
-        
-        Returns the wheel angle in 4096ths.
-        """
-        # FIXME Hard coded to read SM0's FIFO
-        fifo = ptr32(0x50200020)    
-    
-        # Data is actually two 16-bit counters packed into
-        # a 32-bit word.
-        packed_value = fifo[0]
-        raw_invl = packed_value & 0xffff
-        raw_high = (packed_value >> 16) & 0xffff
-        invl = 0xffff - raw_invl
-        high = 0xffff - raw_high
-        angle = high*4119//invl - 15
-        # TODO: <16 means "error"
-        if angle < 0:
-            angle = 0
-        if angle > 4095:
-            angle = 0
-        return angle
-
-    @micropython.viper
-    def update(self, pio):
-        pin_debug.on()
-        try:
-            # FIXME Hard coded to read SM0's FIFO
-            fifo = ptr32(0x50200020)    
-        
-            # Data is actually two 16-bit counters packed into
-            # a 32-bit word.
-            packed_value = fifo[0]
-            raw_invl = packed_value & 0xffff
-            raw_high = (packed_value >> 16) & 0xffff
-            invl = 0xffff - raw_invl
-            high = 0xffff - raw_high
-            angle = high*4119//invl - 15
-            # TODO: <16 means "error"
-            if angle < 0:
-                angle = 0
-            if angle > 4095:
-                angle = 0
-            
-            pole_angle : int = angle * int(self.num_pole_pairs) + int(self.angle_offset)
-            drive_angle : int = pole_angle + int(self.drive_offset)
-
-            power = 1024
-            lut_ptr = ptr16(lut)
-            tap1 = drive_angle & 0xfff
-            tap2 = (drive_angle+1365) & 0xfff
-            tap3 = (drive_angle+2731) & 0xfff
-            duty1 = (lut_ptr[tap1] * power) >> 18 # Extra shift of 6 for PWM TOP scale.
-            duty2 = (lut_ptr[tap2] * power) >> 18
-            duty3 = (lut_ptr[tap3] * power) >> 18
-
-            # Want to do this but each method call costs 10us, which
-            # is far too much.
-            # pwms[0].duty_u16(duty1)
-            # pwms[1].duty_u16(duty2)
-            # pwms[2].duty_u16(duty3)
-
-            # 20 = 2A 0x40050034 LSB
-            # 21 = 2B            MSB
-            # 22 = 3A 0x40050048
-            ptr32(0x40050034)[0] = duty1 | (duty2<<16)
-            ptr32(0x40050048)[0] = duty3
-        except BaseException:
-            global stop 
-            stop = True
-        pin_debug.off()
-
-
 @micropython.native
 def run():
-    yukon.enable_main_output()
-    
-    motor = Motor()
-    motor.calibrate()
-
-    drive_offset = 1024
-    a_was_pressed = False
-
     try:
+        yukon.enable_main_output()
+        
+        motor = Motor(0, 6)
+        motor.calibrate()
+        
+        drive_offset = 1024
+        a_was_pressed = False
+
         # Attach interrupt handler to the "FIFO has data" interrupt. 
         INTR_SM0_RXNEMPTY = 0x001
         rp2.PIO(0).irq(motor.update, trigger=INTR_SM0_RXNEMPTY, hard=True)
@@ -348,13 +370,11 @@ def run():
     finally:
         print("Shutting down")
         rp2.PIO(0).irq(None)
+        motor._set_duty(0,0)
 
 try:
     run()
 
 finally:
     # Put the board back into a safe state, regardless of how the program may have ended
-    for pwm in pwms:
-        pwm.duty_u16(0)
-    yukon.reset()
     gc.collect()
